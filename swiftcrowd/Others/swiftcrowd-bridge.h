660ea9cdbe33f9dabd2dc96d1b77e9e4c4cf4bc3
Emil <emil@appunite.com>
2014-07-05 15:53:10 +0200

    add swiftcrowd-bridge file

---
+++ swiftcrowd/Others/swiftcrowd-bridge.h
@@ -0,0 +1,10 @@
+//
+//  swiftcrowd-bridge.h
+//  swiftcrowd
+//
+//  Created by Emil Wojtaszek on 05/07/14.
+//  Copyright (c) 2014 AppUnite.com. All rights reserved.
+//
+
+#import "AFNetworking.h"
+#import "Mantle.h"
